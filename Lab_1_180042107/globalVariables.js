c81f1a18b16f810a4f8c9126dbd27ac76058888c
smuhtasim <56603189+smuhtasim@users.noreply.github.com>
2021-05-06 16:06:40 +0600

    Completed golbalVariables module

---
+++ Lab_1_180042107/globalVariables.js
@@ -0,0 +1,17 @@
+//__dirname = path to the current directory
+const currentDirectory = __dirname;
+console.log(currentDirectory);
+
+//__filename = filename
+const currentFileName = __filename;
+console.log(currentFileName);
+
+//require = function to provide modules
+const fs = require("fs");
+console.log(fs);
+
+//module = info about current module
+console.log(module);
+
+// process = info about where the program is executed
+console.log(process);
