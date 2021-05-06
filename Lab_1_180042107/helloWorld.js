7a0359b2c53eca5e76cfcb57fcf528f775af23e7
smuhtasim <56603189+smuhtasim@users.noreply.github.com>
2021-05-06 16:05:35 +0600

    Completed helloWorld Module

---
+++ Lab_1_180042107/helloWorld.js
@@ -0,0 +1,14 @@
+const hello2 = function ()
+{
+    console.log("Helloo World");
+}
+
+const name = "Srijon Muhtasim";
+
+const bolo = function (x,y)
+{
+    console.log(x+y);
+}
+module.exports = {helloFunc:hello2, boloFunc:bolo, varName:name};
+
+//console.log(module);
