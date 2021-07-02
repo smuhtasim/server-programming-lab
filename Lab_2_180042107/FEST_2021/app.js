40136b02d8af84c7c3d4102d6686388268f87a91
smuhtasim <56603189+smuhtasim@users.noreply.github.com>
2021-07-02 10:18:20 +0600

    #2

--- Lab_2_180042107/FEST_2021/app.js
+++ Lab_2_180042107/FEST_2021/app.js
@@ -4,6 +4,9 @@ const app = express();
 //Static
 app.use(express.static('public'));
 
+//View Engine
+app.set("view engine", "ejs");
+
 //Routes
 const indexRoutes = require("./routes/index.routes");
 app.use(indexRoutes);
