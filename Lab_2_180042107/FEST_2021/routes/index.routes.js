5e2343aa8893a3e863d6315d365631a016b7c6cd
smuhtasim <56603189+smuhtasim@users.noreply.github.com>
2021-07-02 10:55:15 +0600

    #3

--- Lab_2_180042107/FEST_2021/routes/index.routes.js
+++ Lab_2_180042107/FEST_2021/routes/index.routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 router.get("/",(req,res)=>{
-    res.render("dashboard.ejs");
+    res.render("users/register.ejs");
 });
 
 module.exports = router;
