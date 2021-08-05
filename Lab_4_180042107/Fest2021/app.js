7d11fc2887317327aad9f6f02c651377559b1159
smuhtasim <56603189+smuhtasim@users.noreply.github.com>
2021-08-05 18:31:58 +0600

    lab 4 start

---
+++ Lab_4_180042107/Fest2021/app.js
@@ -0,0 +1,52 @@
+require("dotenv").config()
+const { urlencoded } = require('express');
+const express = require('express');
+const app = express();
+const session = require("express-session");
+const flash = require("connect-flash");
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//Passport Strategy
+require("./config/passport")(passport);
+
+//Connect to Database
+mongoose
+  .connect(process.env.MongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+
+//Static Resources
+app.use(express.static("public"));
+
+//View Engine
+app.set("view engine", "ejs");
+
+//Session and Flash
+app.use(session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+}));
+app.use(flash());
+app.use(passport.initialize())
+app.use(passport.session());
+
+//Body Parser
+app.use(express.urlencoded({extended:false}));
+
+//Routes
+const indexRoutes = require("./routes/index.routes");
+const userRoutes = require("./routes/users.routes");
+const { Session } = require('express-session');
+app.use(indexRoutes);
+app.use("/users", userRoutes);
+
+module.exports = app;
